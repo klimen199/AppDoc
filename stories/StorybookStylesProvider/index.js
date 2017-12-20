7441a22a0f1153e02d424769eebe40ff702fa9ff
WladisLove <klimen97@gmail.com>
2017-12-20 14:49:34 +0300

    initial commit

---
+++ stories/StorybookStylesProvider/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { LocaleProvider } from 'antd'
+import ruRu from 'antd/lib/locale-provider/ru_RU'
+
+import './storybook-master.less'
+
+const StylesProvider = props => {
+    const { children } = props
+
+    return <LocaleProvider locale={ruRu}>{children}</LocaleProvider>
+}
+
+StylesProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+
+export default StylesProvider
---
+++ stories/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react'
+
+import StorybookStylesProvider from './StorybookStylesProvider'
+
+const LocalProviderDecorator = story => (
+    <div>
+        <StorybookStylesProvider>{story()}</StorybookStylesProvider>
+    </div>
+)
+
+const loadStories = () => {
+    addDecorator(LocalProviderDecorator)
+
+    require('../src/components/Button/stories')
+}
+
+configure(loadStories, module)
