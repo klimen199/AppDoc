fff0ad8fd7a4e020ed5519c6f0e0bb4aa2492823
WladisLove <klimen97@gmail.com>
2017-12-26 14:15:11 +0300

    Modal base

---
+++ src/components/Modal/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+import { Modal as AntModal } from 'antd';
+import './styles.css'
+
+class Modal extends React.Component{
+
+
+    render(){
+
+        const {visible, title} = this.props;
+
+        return (
+            <AntModal visible={visible}
+                      title={title}
+                      footer={null}>
+                {this.props.children}
+            </AntModal>
+        )
+    }
+}
+
+Modal.propTypes = {
+    visible: PropTypes.bool,
+    title: PropTypes.string,
+}
+
+Modal.defaultProps = {
+    visible: false,
+    title: '',
+}
+
+export default Modal
---
+++ src/components/Modal/stories/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Button from '../../Button'
+import Modal from '../';
+
+storiesOf('Modal', module)
+    .add('Modal', () => (
+        <div>
+            <Modal title="Basic Modal"
+                   visible={true}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
+        </div>
+    ))
