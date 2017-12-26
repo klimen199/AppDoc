c68f1cd4fa4318b773738f5eff057dbdc385f442
antonbuyuk <anton.buyuk@abiatec.com>
2017-12-26 15:14:21 +0300

    Input Search fix #32

---
+++ src/components/Input/index.js
@@ -0,0 +1,4 @@
+import { Input , Icon} from 'antd'
+import './style.css'
+
+export default Input
---
+++ src/components/Input/stories/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import Input from '../';
+
+storiesOf('Inputs', module)
+    .add('Search', () => (
+        <div>
+           <Input.Search
+		      placeholder="Поиск..."
+		      onSearch={value => console.log(value)}
+		    />
+        </div>
+    ))
