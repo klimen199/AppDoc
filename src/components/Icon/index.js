a2167e054b124198b0358f17797edb16877f64e4
WladisLove <klimen97@gmail.com>
2017-12-28 11:15:13 +0300

    Icon by Vlad

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,31 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import cn from 'classnames'
 
 import { Icon as AntdIcon } from 'antd'
 
-import { getIcon, iconsList } from './IconsList'
-
 const Icon = props => {
-    const { svg, type, size, style, onClick } = props;
+    const { svg, type, size, onClick } = props;
 
     if (!svg)
-        return (<AntdIcon onClick={onClick} type={type} style={{fontSize:size}} />);
+        return (<AntdIcon onClick={onClick}
+                          type={type}
+                          style={{fontSize:size}} />);
 
-    return <span onClick={onClick}>{getIcon(type, size)}</span>
+    const clName = cn('icon', `icon-${type}`)
+
+    return <span onClick={onClick}
+                 className={clName}
+                 style={{fontSize:size}}/>
 };
 
 Icon.propTypes = {
     svg: PropTypes.bool,
     type: PropTypes.string.isRequired,
     size: PropTypes.number,
-    style: PropTypes.object,
     onClick: PropTypes.func,
 };
 
 Icon.defaultProps = {
     svg: false,
     size: 36,
-    style: {},
     onClick: () => {},
 };
 
--- src/components/Icon/stories/index.js
+++ src/components/Icon/stories/index.js
@@ -2,29 +2,23 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import Icon from '../'
-import { iconsList } from '../IconsList'
+import {iconList} from './iconList'
 
-const iconTypes = Object.keys(iconsList);
+const styles = {
+    display:'inline-block',
+    margin:5,
+    padding:5,
+    border:'1px black solid',
+    textAlign:'center',
+};
 
-const showIcons = () => {
-    return (<div>
-        {iconTypes.map(type => (
-            <div key={type}>
-                <Icon svg type={type} size={40} />
+storiesOf('Icon', module)
+    .add('Custom SVG icon', () => <div>
+        {iconList.map(type => (
+            <div key={type} style={styles}>
+                <Icon svg type={type} size={30} />
                 <p>{type}</p>
             </div>
         ))}
-    </div>);
-};
-
-import './lol.css'
-
-storiesOf('Icon', module)
-    .add('Custom SVG icon', showIcons)
-    .add('test', () => (
-        <div className="lol">
-            <Icon svg type="arrowLeft"/>
-            <span>lolol </span>
-        </div>
-    ))
+    </div>)
     .add('Antd icons', () => <Icon type="check" />);
