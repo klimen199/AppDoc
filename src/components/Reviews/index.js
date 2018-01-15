7fdda310f1ecca50759a79a9af30ac2cfc0b10b6
WladisLove <klimen97@gmail.com>
2018-01-15 15:57:46 +0300

    Reviews fix

--- src/components/Reviews/index.js
+++ src/components/Reviews/index.js
@@ -1,45 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-import Rewiew from '../Review'
+import Review from '../Review'
 import Card from '../Card'
 import Icon from '../Icon'
 
 import './style.css'
-import '../../icon/style.css'
 
 class Reviews extends React.Component{
+
+    reviewRender = (dataArr) => {
+        let revArr = [];
+
+        dataArr.map((item) => {
+            revArr.push(<Review {...item} key={item.id}/>)
+        });
+
+        return revArr;
+    };
 
     render(){
 
         return (
-            <div className='rewiews-all'>
-                <Card title="Новые отзывы" className="reviews" extra={<a href="#"><Icon type="chat" svg size={20}/> Все отзывы</a>}>
-                    <Rewiew 
-                        name="Иванова А. К." 
-                        rewText="Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. " 
-                        img="https://www.proza.ru/pics/2017/06/03/1990.jpg" 
-                        time="5"
-                        unit="часов"
-                    />
-
-                    <Rewiew 
-                        name="Иванова А. К." 
-                        rewText="Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты." 
-                        img="https://www.proza.ru/pics/2017/06/03/1990.jpg" 
-                        time="5"
-                        unit="часов"
-                    />
-
-                    <Rewiew 
-                        name="Иванова А. К." 
-                        rewText="Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты." 
-                        img="https://www.proza.ru/pics/2017/06/03/1990.jpg" 
-                        time="5"
-                        unit="часов"
-                    />
-                  </Card>
-            </div>
+                <Card title="Новые отзывы"
+                      className="reviews"
+                      extra={<a href="#"><Icon type="chat" svg size={20}/> Все отзывы</a>}>
+                    {this.reviewRender(this.props.data)}
+                </Card>
         )
     }
 }
--- src/components/Reviews/stories/mock-data.js
+++ src/components/Reviews/stories/mock-data.js
@@ -3,15 +3,52 @@ const hour = minute * 60;
 const day = hour * 24;
 const now = Date.now();
 
-export const dataArr = [
-    {
-        id: 0,
-        parentId: null,
+export const dataArr = [{
+        id: 1,
         author: "Иванова А. К.",
         avatar: "https://www.proza.ru/pics/2017/06/03/1990.jpg",
         text: "Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. ",
-        date: `${new Date(now - 17 * minute)}`,
+        date: new Date(now - 17 * minute),
+        treatmentDate: '13.10.2017',
+        rate: 4,
+    }, {
+        id: 7,
+        author: "Иванова А. К.",
+        avatar: "https://www.proza.ru/pics/2017/06/03/1990.jpg",
+        text: "Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. ",
+        date: new Date(now - 3 * hour),
+        treatmentDate: '13.10.2017',
+        rate: 4,
+    }, {
+        id: 8,
+        author: "Иванова А. К.",
+        avatar: "https://www.proza.ru/pics/2017/06/03/1990.jpg",
+        text: "Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. ",
+        date: new Date(now - 9 * hour),
+        treatmentDate: '13.09.2017',
+        rate: 4,
+    }, {
+        id: 9,
+        author: "Иванова А. К.",
+        avatar: "https://www.proza.ru/pics/2017/06/03/1990.jpg",
+        text: "Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. ",
+        date: new Date(now - 3 * day),
         treatmentDate: '13.09.2017',
         rate: 4,
     },
 ];
+
+
+// const data1 = {
+//     author: "Иванова А. К.",
+//     avatar: "https://www.proza.ru/pics/2017/06/03/1990.jpg",
+//     text: "Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. ",
+//     date: date1,
+//     treatmentDate: '13.10.2017',
+//     rate: 4,
+// };
+// const data2 = {
+//     text: "Далеко-далеко. Максимально далеко и еще чуть-чуть дальше за словесными горами в стране гласных и согласных живут рыбные тексты. ",
+//     date: date1,
+//     secondary: true,
+// };
