a1e5ed09746f30ab71fd7a1c93ac2d7c8184348d
antonbuyuk <anton.buyuk@abiatec.com>
2018-01-15 17:32:18 +0300

    fix defaultProps

--- src/components/ProfilePatient/stories/index.js
+++ src/components/ProfilePatient/stories/index.js
@@ -6,6 +6,19 @@ import ProfilePatient from '../';
 storiesOf('ProfilePatient', module)
     .add('ProfilePatient', () => (
         <div>
-            <ProfilePatient/>
+            <ProfilePatient
+            	secondname="Петров-Иванов"
+            	firstname="Александр"
+            	patronymic="Константинович"
+            	img="https://24smi.org/public/media/resize/660x-/person/2017/10/25/cdRRFH0JWoYv_supermen.jpg"
+            	status="offline"
+            	lastDate="01.01.2000"
+            	doctorType="врач-терапевт"
+            	doctor="Тимошенко Т.И"
+            	birthday="31.12.1999"
+            	age="18"
+            	height="187"
+            	weight="85"
+            />
         </div>
     ))
