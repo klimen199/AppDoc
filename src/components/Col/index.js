73038dfa66334580878eab624f2f8ecb39f85e97
WladisLove <klimen97@gmail.com>
2018-01-22 16:52:44 +0300

    Row & Col custome

--- src/components/Col/index.js
+++ src/components/Col/index.js
@@ -1,3 +1,8 @@
-import {Col} from 'antd'
+import React from 'react'
+import {Col as AntCol} from 'antd'
+
+const Col = (props) => {
+    return (<AntCol span={(props.span * 2)} {...props}>{props.children}</AntCol>)
+};
 
 export default Col;
--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import {Row as AntRow} from 'antd'
 
 const Row = (props) => {
