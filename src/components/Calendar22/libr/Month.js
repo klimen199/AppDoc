d8571fb8c3e0d5605d9b942067a59f5c02e7b645
WladisLove <klimen97@gmail.com>
2018-01-24 13:45:34 +0300

    Calendar month ready (again)

--- src/components/Calendar22/libr/Month.js
+++ src/components/Calendar22/libr/Month.js
@@ -3,6 +3,8 @@ import React from 'react'
 import { findDOMNode } from 'react-dom'
 import cn from 'classnames'
 
+import dates from './utils/dates'
+import localizer from './localizer'
 import chunk from 'lodash/chunk'
 
 import { navigate, views } from './utils/constants'
