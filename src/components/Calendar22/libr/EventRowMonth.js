1a645cb98aab0d5b3146e112e9391138d3b90555
WladisLove <klimen97@gmail.com>
2018-01-25 20:47:06 +0300

    Calendar ready

--- src/components/Calendar22/libr/EventRowMonth.js
+++ src/components/Calendar22/libr/EventRowMonth.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { eventLevels } from './utils/eventLevels'
 import cn from 'classnames'
-import Hoc from '../../Hoc'
 import Icon from '../../Icon'
 
 
@@ -14,24 +13,28 @@ class EventRowMonth extends React.Component {
     isSegmentInSlot = (seg, slot) => seg.left <= slot && seg.right >= slot
 
     renderRow = (row) => {
-        console.log('Render Row', row)
-        let rez = row.map((el, i) => {
+
+        return row.map((el, i) => {
             let cellClass;
 
             let div = el ? (
-                cellClass = cn(
-                    'rbc-row-segment',
-                    'rbc-month-receptionNum',
-                    {'rbc-month-receptionNum-coming':
-                        this.now.setHours(0,0,0,0) <= el.date.setHours(0,0,0,0)}),
-                    <div key={'_lvl_' + i}
-                         className={cellClass}
-                         style={{width: `${100 / 7}%`}}>
-                        <div className="icon-count">
-                            <Icon type="user" size={28}/>
-                        </div> {el.gap}
-                    </div>
-            ) :
+                    cellClass = cn(
+                        'month-row-segment',
+                        {'month-row-segment-coming':
+                        this.now.getDate() <= el.date.getDate() &&
+                        this.now.getMonth() <= el.date.getMonth() &&
+                        this.now.getYear() <= el.date.getYear()}),
+
+                        <div key={'_lvl_' + i}
+                             className={cellClass}
+                             style={{width: `${100 / 7}%`}}>
+                            <div className="month-row-segment-content">
+                                <div className="icon-count">
+                                    <Icon type="user" size={28}/>
+                                </div> {el.gap}
+                            </div>
+                        </div>
+                ) :
                 <div key={'_lvl_' + i}
                      className="rbc-row-segment"
                      style={{width: `${100/7}%`}}/>
@@ -39,13 +42,11 @@ class EventRowMonth extends React.Component {
 
             return (div)
         });
-
-        return rez;
     }
 
     render() {
-        let { segments, slots: slotCount = 7 } = this.props;
-        let rowSegments = eventLevels(segments).levels[0];
+        let { segments, slots: slotCount = 7 } = this.props
+        let rowSegments = eventLevels(segments).levels[0]
         if (!rowSegments) rowSegments = [];
 
         let current = 1,
@@ -57,7 +58,7 @@ class EventRowMonth extends React.Component {
                 rowSegments.filter(seg => this.isSegmentInSlot(seg, current))[0] || {}
 
             if (!event) {
-                row.push(null);
+                row.push(null)
                 current++;
                 continue
             }
@@ -80,9 +81,12 @@ class EventRowMonth extends React.Component {
             }
             currentInSeg++;
             current++
+
         }
 
-        return <div className="rbc-row">{this.renderRow(row)}</div>
+        return <div className="month-row">
+            {this.renderRow(row)}
+                </div>
     }
 
 }
