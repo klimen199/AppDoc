3e2f1f76b9999987c2d33405911850799ade1500
antonbuyuk <anton.buyuk@abiatec.com>
2018-01-25 13:17:40 +0300

    Add Calendar Popover

--- src/components/Popover/stories/index.js
+++ src/components/Popover/stories/index.js
@@ -8,7 +8,7 @@ import Link from '../../Links'
 import Button from '../../Button'
 
 const content = (
-  <div>
+  <div className='default-popover'>
     <Link btnText="Прикрепленный файл с длинным предлинным названием.doc"
           size="default" 
           type="link"
@@ -44,11 +44,53 @@ const content = (
   </div>
 );
 
+const content2 = (
+  <div className='calendar-body'>
+    <a className='calendar-name'>Иванова Александра</a>
+    <div className='calendar-date'>23 ноября  
+      <Button 
+        size='file'
+        type='file'
+        icon='telephone'
+        svg
+        iconSize={21}
+      />
+    </div>
+    
+    <div className='calendar-time'>08:15 - 08:20</div>
+    <div className='calendar-text'>Консультация по результатам анализа крови.</div>
+
+    <div className='calendar-block'>
+      <Button 
+        size='file'
+        type='file'
+        icon='mail'
+        svg
+        iconSize={16}
+      />
+      <Button 
+        size='file'
+        type='file'
+        icon='circle_close'
+        svg
+        iconSize={18}
+      />
+    </div>
+  </div>
+);
+
 storiesOf('Popover', module)
     .add('Popover', () => (
         <div style={{ padding: '30px' }}>
             <Popover placement="bottomRight" content={content} trigger="click">
                  <Icon svg type={"file-download popover-nun"} size={30}  num={3}/>
+            </Popover>
+        </div>
+    ))
+    .add('CalendarPopover', () => (
+        <div style={{ padding: '30px' }}>
+            <Popover className='calendar-popover' placement="rightTop" content={content2} trigger="click">
+                 <Button btnText='Right' />
             </Popover>
         </div>
     ))
