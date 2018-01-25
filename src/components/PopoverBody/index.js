493895ffccb4f0e19d2bc4f5c498a0e8f7365cbc
WladisLove <klimen97@gmail.com>
2018-01-25 21:06:10 +0300

    Update index.js

--- src/components/PopoverBody/index.js
+++ src/components/PopoverBody/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import cn from 'classnames'
-import { action } from '@storybook/addon-actions';
 import Icon from '../Icon'
 import Button from '../Button'
 
