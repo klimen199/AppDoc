d4c7879e92a91db0dbae9ce2aa3f367b854cb6e2
WladisLove <klimen97@gmail.com>
2018-02-08 14:07:11 +0300

    fix null set of rangeSet in CancelVisitModal & ReceptionsScheduleModal

--- src/components/ReceptionsScheduleModal/stories/index.js
+++ src/components/ReceptionsScheduleModal/stories/index.js
@@ -11,28 +11,7 @@ storiesOf('Modal - ReceptionsScheduleModal', module)
     .add('modal', () => (
         <div>
             <ReceptionsScheduleModal visible={true}
-                                     dateSet={{
-                                             defaultEndValue: moment(new Date(2017,11,11)),
-                                             defaultStartValue: moment(new Date(2017,11,11))
-                                         }}
-                                     timeSetCall = {[
-                                         {
-                                             defaultEndValue: moment(new Date(2017,11,11,14,50), "YYYY-MM-DD HH:mm"),
-                                             defaultStartValue: moment(new Date(2017,11,11,14,55), "YYYY-MM-DD HH:mm"),
-                                         },{
-                                         defaultEndValue: moment("2017-10-20 14:30", "YYYY-MM-DD HH:mm"),
-                                         defaultStartValue: moment("2017-10-20 14:40", "YYYY-MM-DD HH:mm"),
-                                     }
-                                     ]}
-                                     timeSetReception= {[
-                                         {
-                                             defaultEndValue: moment("2017-10-20 15:50", "YYYY-MM-DD HH:mm"),
-                                             defaultStartValue: moment("2010-10-20 15:55", "YYYY-MM-DD HH:mm"),
-                                         },{
-                                             defaultEndValue: moment("2017-10-20 16:30", "YYYY-MM-DD HH:mm"),
-                                             defaultStartValue: moment("2017-10-20 16:40", "YYYY-MM-DD HH:mm"),
-                                         }
-                                     ]}
+
                                      selOptions={options}/>
         </div>
     ));
