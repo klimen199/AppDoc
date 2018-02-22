5e9c27cfa5bbf829a2c0fa238e7a6223840d88df
WladisLove <klimen97@gmail.com>
2018-02-22 10:30:23 +0300

    add limit prop to UploadBig

--- src/components/UploadBig/index.js
+++ src/components/UploadBig/index.js
@@ -1,51 +1,57 @@
 import React from 'react'
-import { Upload } from 'antd';
+import {Upload} from 'antd';
+import PropTypes from 'prop-types'
 
 import Icon from '../Icon'
 
 import './styles.css'
 
 class UploadBig extends React.Component {
-  state = {
-    previewVisible: false,
-    previewImage: '',
-    fileList: [],
-  };
+    state = {
+        previewImage: '',
+        fileList: [],
+    };
 
-  handleCancel = () => this.setState({ previewVisible: false })
+    handlePreview = (file) => {
+        this.setState({
+            previewImage: file.url || file.thumbUrl,
+        });
+    };
 
-  handlePreview = (file) => {
-    this.setState({
-      previewImage: file.url || file.thumbUrl,
-      previewVisible: true,
-    });
-  }
+    handleChange = ({fileList}) => this.setState({fileList});
 
-  handleChange = ({ fileList }) => this.setState({ fileList })
+    render() {
+        const {previewImage, fileList, name} = this.state;
 
-  render() {
-    const { previewVisible, previewImage, fileList, name } = this.state;
-    const uploadButton = (
-      <div className='upload-btn' title='Загрузить файл'>
-        <Icon type="add-button" size={40} svg/>
-      </div>
-    );
-    return (
-      <div className="upload">
-        <Upload className="upload-block"
-          action=""
-          listType="picture-card"
-          fileList={fileList}
-          onPreview={this.handlePreview}
-          onChange={this.handleChange}
-          name={name}
-        >
-          {fileList.length >= 100 ? null : uploadButton}
-        </Upload>
-      </div>
-    );
-  }
+
+        const uploadButton = (
+            <div className='upload-btn' title='Загрузить файл'>
+                <Icon type="add-button" size={40} svg/>
+            </div>
+        );
+        return (
+            <div className="upload">
+                <Upload className="upload-block"
+                        action=""
+                        listType="picture-card"
+                        fileList={fileList}
+                        onPreview={this.handlePreview}
+                        onChange={this.handleChange}
+                        name={name}
+                >
+                    {fileList.length >= this.props.limit ? null : uploadButton}
+                </Upload>
+            </div>
+        );
+    }
 }
 
+UploadBig.propTypes = {
+    limit: PropTypes.number,
+};
+
+UploadBig.defaultProps = {
+    limit: 5,
+};
 
 export default UploadBig;
