6d8f2901c8ee6859232029a00a5569cdd876966c
WladisLove <klim12121997@yandex.by>
2018-04-03 13:36:02 +0300

    add func to set focus in Input; add algo for searching

--- src/components/AutoComplete/index.js
+++ src/components/AutoComplete/index.js
@@ -12,17 +12,58 @@ class AutoComplete extends React.Component{
     constructor(props){
         super(props);
         this.state ={
-            onFocus: false,
-            searchString: ""
+            inputFocus: false,
+            itemFocus: false,
+            
+            searchRes: this.props.data,
         }
+        this.input;
     }
 
-    focusHandler = (isFocused) => {
-        this.setState(prev => ({onFocus: isFocused}))
-    };
+    searchHandler = (e) => {
+        let search = () => {
+            this.setState({inputFocus: true});
+            let str = (e.target.value).toLowerCase();
+            let newArr = this.props.data.filter(elem => {
+                console.log(str.split(' ').length > 1 )
+                return str.split(' ').length === 1 
+                    ? search1word(elem, str)
+                    : searchSeveralWords(elem, str);
+                
+            })
+    
+            this.setState({searchRes: newArr})
 
-    searchHandler = () => {
+            function search1word(elem, str){
+                let fioArr = elem.name.toLowerCase().split(' ');
+                for (let i =0, len = fioArr.length; i < len; i++){
+                    if(fioArr[i].indexOf(str) === 0){
+                        return true;
+                    }
+                }
+            }
+            function searchSeveralWords(elem, str){
+                let searchArr = str.split(' ');
+                for(let j = 0, len = searchArr.length; j < len; j++){
+                    if (!search1word(elem, searchArr[j])){
+                        return false;
+                    }
+                    else if(j == len - 1) return true;
+                }
+                
+            }
+        }
 
+        e.target.value.length > 0 
+            ? search() 
+            : this.setState({
+                inputFocus: false,
+                searchRes: this.props.data,
+            });
+        
+       
+
+        
     }
 
 
@@ -32,7 +73,7 @@ class AutoComplete extends React.Component{
         dataArr.map((item, index) => {
             patientsArr.push(<AddNewPatientItem {...item} 
                                                 isSearchItem={true}
-                                                onAdd = {(id) => {this.props.onAdd(id); this.focusHandler(false)}}
+                                                onAdd = {(id) => {this.props.onAdd(id)}}
                                                 key={item.id + ''+index}/>)
         });
 
@@ -43,28 +84,49 @@ class AutoComplete extends React.Component{
         
         const { data, collapsed} = this.props;
         const rootClass = cn('auto__complete');
-        const resultClass = this.state.onFocus ? 'auto__complete-result auto__complete-result-focus' : 'auto__complete-result';
+        const resultClass = (this.state.itemFocus || this.state.inputFocus)? 'auto__complete-result auto__complete-result-focus' : 'auto__complete-result';
+
+        console.log(this.state.inputFocus, this.state.itemFocus)
 
         return (
-            <div className={rootClass} 
-                onFocus={(e) => console.log(e.target)}
-            >
+            <div className={rootClass}>
                 <div className='auto__complete-search'>
                     <Input 
                         placeholder='Поиск'
-                        onChange={this.searchHandler}
+                        onChange={(e) => this.searchHandler(e)}
+                        onBlur={()=> {
+                            console.log('input blur')
+                            //this.setState({inputFocus: false})
+                        }}
+                        ref = {inp => {this.input = inp}}
                        
                     />
                 </div>
-                <div className={resultClass}>
-                    <div className='auto__complete-title'>Результаты поиска <span className='auto__complete-count'>{data.length}</span></div>
+                <div className={resultClass}
+
+                    
+                    onBlur={()=> {
+                        console.log('items blur')
+                        this.setState({itemFocus: false})
+                    }}
+                    onFocus={() => {
+                        console.log('items focus')
+                        this.setState({itemFocus: true})
+                    }}
+                >
+                    <div className='auto__complete-title' onClick={() => {
+                            this.input.setFocus(true)
+                        }}>
+                        Результаты поиска 
+                        <span className='auto__complete-count'>{this.state.searchRes.length}</span>
+                    </div>
                     <ScrollArea
                             speed={1}
                             className="auto__complete-list"
                             contentClassName="content"
                             horizontal={false}
                     >
-                        {this.patientsRender(this.props.data)}
+                        {this.patientsRender(this.state.searchRes)}
                     </ScrollArea>
                 </div>
             </div>
