4e5f8c45f4a3a3bb2ce247c7b968dd64e726caec
Alex <enavals1@mail.ru>
2018-05-14 11:51:36 +0300

    good skroll

--- src/components/NotificationApp/stories/index.js
+++ src/components/NotificationApp/stories/index.js
@@ -10,7 +10,7 @@ storiesOf('NotificationApp', module)
     .add('NotificationApp', () => (
         <div>
             <div style={{ width: '300px', height: '1000px'}}> dsd</div>
-            <div style={{ padding: '30px', marginLeft:'300px',marginTop:'100px', position:'fixed' , left:'0', top:'0' }}>
+            <div style={{ padding: '30px', marginLeft:'600px',marginTop:'100px', position:'fixed' , left:'0', top:'0' }}>
                 <NotificationApp  data={notificationArr} />
             </div>
             <div style={{ width: '300px', height: '1000px'}}> dsd</div>
