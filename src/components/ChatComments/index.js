b512236ecc9a97d18099ef97250934e4340ee0bf
wladislove <klimen97@gmail.com>
2018-06-15 15:30:34 +0300

    ChatComments: empty case

--- src/components/ChatComments/index.js
+++ src/components/ChatComments/index.js
@@ -1,42 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-import cn from 'classnames'
 
 import DownloadLink from '../DownloadLink'
+import Hoc from "../Hoc"
 
 import './style.css'
 import '../../icon/style.css'
 
-class ChatComments extends React.Component{
-    render(){
-        const {comments, files} = this.props;
-        const rootClass = cn('message__comments-area');
-
+const ChatComments = props => {
+        const {comments, files} = props;
 
         return (
-            <div className={rootClass}>
-                <div className='message__comments-comments'>
-                    {comments}
-                </div>
-                <div className='message__comments-attachment'>
-                    {
-                        files.map((el,i) => {
-                            return (<DownloadLink
-                                {...el}
-                                size="default" 
-                                type="link"
-                                download
-                                svg
-                                icon="file"
-                                iconSize={11}
-                                key={i}
-                            />)
-                        })
-                    }
-                </div>
-            </div>
+            <Hoc>
+                {
+                    comments !== "" && files.length !== 0 &&
+                    (
+                            <div className='message__comments-area'>
+                                <div className='message__comments-comments'>
+                                    {comments}
+                                </div>
+                                <div className='message__comments-attachment'>
+                                    {
+                                        files.map((el,i) => {
+                                            return (<DownloadLink
+                                                {...el}
+                                                size="default" 
+                                                type="link"
+                                                download
+                                                svg
+                                                icon="file"
+                                                iconSize={11}
+                                                key={i}
+                                            />)
+                                        })
+                                    }
+                                </div>
+                            </div>
+                    )
+                }
+            </Hoc>
         )
-    }
 }
 
 ChatComments.propTypes = {
