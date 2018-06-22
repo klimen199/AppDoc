8b74e1ad7912d1a8359d2814e79c724f216b7c30
Vladimir Zubritskiy <Vovavipse@gmail.com>
2018-06-22 16:41:12 +0300

    added scroll

--- src/components/NotificationCard/index.js
+++ src/components/NotificationCard/index.js
@@ -4,39 +4,62 @@ import Card from '../Card'
 import NotificationItem from '../Notification'
 
 import './style.css'
+import ScrollArea from 'react-scrollbar'
 
+class NotificationCard extends React.Component{
+    constructor(props) {
+        super(props);
+    }
 
-const NotificationCard = (props) => {
-
-    const notificationRender = (dataArr) => {
+    notificationRender = (dataArr) => {
         let notificationArr = [];
         dataArr.map((item, index) => {
             notificationArr.push(<NotificationItem {...item} 
-                getId={props.getId} 
+                getId={this.props.getId}
                 key={item.id}/>)
         });
         return notificationArr;
     };
 
-    
+    render() {
         return (
+
             <div className='notification-card'>
-                <Card title="Уведомления">
-                    {notificationRender(props.data)}
-                </Card>
+
+
+                    <Card title="Уведомления">
+                        <ScrollArea
+                            speed={1}
+                            className="scroll"
+                            contentClassName="content"
+                            horizontal={false}
+                            verticalScrollbarStyle = {{
+                              background: "#fdc401",
+                            }}
+                            verticalContainerStyle = {{
+                                right: 5
+                            }}
+                        >
+                            {this.notificationRender(this.props.data)}
+                        </ScrollArea>
+                    </Card>
+
             </div>
+
         );
+    }
+
     
 }
 
 NotificationCard.propTypes ={
     data: PropTypes.arrayOf(PropTypes.object),
     getId: PropTypes.func,
-}
+};
 
 NotificationCard.defaultProps = {
     data: [],
     getId: () => {},
-}
+};
 
 export default NotificationCard
