95900adc77ec28dd652df76ae96a6aac5970de02
Vladimir Zubritskiy <Vovavipse@gmail.com>
2018-06-26 13:01:19 +0300

    fixed v1

--- src/components/PatientTableItem/index.js
+++ src/components/PatientTableItem/index.js
@@ -8,29 +8,15 @@ import Rate from '../Rate'
 import Icon from '../Icon'
 import Popover from '../Popover'
 import ProfileAvatar from '../ProfileAvatar'
-import NewVisitModalPage from '../NewVisitModalPage'
-import NewMessageModal from '../NewMessageModal'
-
 import './style.css'
 import '../../icon/style.css'
 
 class PatientTableItem extends React.Component{
-    state = {
-        modal1Visible: false,
-        modal2Visible: false,
-    }
-    setModal1Visible(modal1Visible) {
-        this.setState({ modal1Visible });
-    }
-    setModal2Visible(modal2Visible) {
-        this.setState({ modal2Visible });
-    }
-
-
     render(){
+        console.log(this.props);
         const { id, name, age, size, time, date, online, avatar, onGoto, dateend, datestart} = this.props;
         const rootClass = cn('patient-item');
-        
+
         return (
             <div className={rootClass}>
                 <div className="flex-col"><ProfileAvatar owner="patient" online={online} img={avatar} size={size}/></div>
@@ -43,15 +29,15 @@ class PatientTableItem extends React.Component{
                 <div className="flex-col">
                     <div className="patient-item-title">Последний приём:</div>
                     <div className="patient-item-time">
-                        {moment.unix(datestart).format('DD.MM.YYYY')} 
+                        {moment.unix(datestart).format('DD.MM.YYYY')}
                         <br/>
                         {moment.unix(datestart).format('HH:mm')}
                         -
-                        {moment.unix(dateend).format('HH:mm')}                        
+                        {moment.unix(dateend).format('HH:mm')}
                     </div>
                 </div>
                 <div className="flex-col">
-                    <Button onClick={() => this.setModal1Visible(true)}
+                    <Button onClick={() => this.props.setModal1Visible(true, id, name)}
                         btnText='записать на прием'
                         size='default'
                         type='float'
@@ -62,7 +48,7 @@ class PatientTableItem extends React.Component{
                 <div className="flex-col">
                     <Button onClick={(e) => {
                             e.preventDefault();
-                            this.setModal2Visible(true)
+                            this.setModal2Visible(true, id, name)
                         }}
                         size='file'
                         type='file'
@@ -81,30 +67,7 @@ class PatientTableItem extends React.Component{
                         onClick = {() => this.props.onDelete(this.props.id)}
                     />
                 </div>
-                <NewMessageModal 
-                    visible={this.state.modal2Visible}
-                    onSend={(a) => {
-                        this.setModal2Visible(false);
-                        //console.log(a)
-                        this.props.onNewMessage(a)
-                    }}
-                    onCancel={() => this.setModal2Visible(false)}
-                    userName={name}
-                    id={this.props.id}
-                />
-                <NewVisitModalPage 
-                    visible={this.state.modal1Visible}
-                    onSave={(a) => {
-                        this.setModal1Visible(false);
-                        this.props.onNewVisit(a)
-                    }}
-                    onCancel={() => this.setModal1Visible(false)}
-                    userName={name}
-                    availableArea={this.props.availableArea}
-                    onChangeDate={this.props.onChangeDate}
 
-                    id={id}
-                />
             </div>
         )
     }
